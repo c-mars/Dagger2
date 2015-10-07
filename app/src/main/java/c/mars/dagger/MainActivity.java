514ffa7c383f12d85143225d37fe6272131db5ec
Constantine Mars <constantine.mars@gmail.com>
2015-10-08 01:02:14 +0300

    optional example

--- app/src/main/java/c/mars/dagger/MainActivity.java
+++ app/src/main/java/c/mars/dagger/MainActivity.java
@@ -3,6 +3,8 @@ package c.mars.dagger;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.common.base.Optional;
+
 import org.joda.time.DateTime;
 import org.joda.time.MutableDateTime;
 
@@ -10,8 +12,6 @@ import javax.inject.Inject;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import rx.functions.Action1;
-import rx.functions.Func2;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
         Timber.d(model.desc());
 
-        merge();
+        optional();
+
+//        merge();
 
 //        Random random=new Random();
 //        for(int i=0;i<10;i++){
@@ -40,17 +42,18 @@ public class MainActivity extends AppCompatActivity {
 //        timeDemo();
     }
 
+    private void optional() {
+        Optional<Integer> v = Optional.fromNullable(5);
+        Timber.d(v.or(-1).toString());
+
+        Optional<Integer> n = Optional.fromNullable(null);
+        Timber.d(n.or(-1).toString());
+    }
+
     private void merge() {
-        rx.Observable a = rx.Observable.from(new Integer[]{1, 2, 3, 4});
-        rx.Observable b = rx.Observable.from(new String[]{"a", "b", "c", "d"});
-        rx.Observable.zip(a, b, new Func2<Integer, String, ValuesPair>() {
-            @Override
-            public ValuesPair call(Integer v, String d) {
-                return new ValuesPair(v, d);
-            }
-        }).subscribe((Action1) valuesPair -> {
-            Timber.d(valuesPair.toString());
-        });
+        rx.Observable<Integer> a = rx.Observable.from(new Integer[]{1, 2, 3, 4});
+        rx.Observable<String> b = rx.Observable.from(new String[]{"a", "b", "c", "d"});
+        rx.Observable.zip(a, b, ValuesPair::new).subscribe(valuesPair -> Timber.d(valuesPair.toString()));
     }
 
     private void timeDemo(){
